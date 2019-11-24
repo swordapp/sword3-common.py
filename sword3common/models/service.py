48c00920b8542c69c6ae8ec288d3c74f1f674a5d
Richard Jones <richard@cottagelabs.com>
2019-11-24 21:09:37 +0000

    add full version of refactored dataobj as seamless mixin; we're ready to do fuller testing on this and move to that as the main object backing

--- sword3common/models/service.py
+++ sword3common/models/service.py
@@ -1,8 +1,8 @@
 from sword3common.lib.dataobj import DataObj
 
-class ServiceDcument(DataObj):
+class ServiceDocument(DataObj):
     def __init__(self, raw=None):
-        super(ServiceDcument, self).__init__(raw, struct=SERVICE_STRUCT, expose_data=True)
+        super(ServiceDocument, self).__init__(raw, struct=SERVICE_STRUCT, expose_data=True)
 
 
 SERVICE_STRUCT = {
@@ -23,6 +23,7 @@ SERVICE_STRUCT = {
         "stagingMaxIdle" : {"coerce" : "integer"},
         "byReferenceDeposit" : {"coerce" : "bool"},
         "onBehalfOf" : {"coerce" : "bool"},
+        "parent" : {"coerce" : "unicode"}
     },
     "lists" : {
         "accept" : {"contains" : "field", "coerce" : "unicode"},
@@ -53,3 +54,31 @@ SERVICE_STRUCT = {
         }
     }
 }
+
+from sword3common.lib.seamless import SeamlessMixin, SeamlessData
+
+def makeSeamlessService(raw):
+    return SeamlessService(raw)
+
+def makeCP(raw):
+    return SeamlessData(raw)
+
+def extractCP(refined):
+    return refined.data
+
+class SeamlessService(SeamlessMixin):
+    __SEAMLESS_STRUCT__ = SERVICE_STRUCT
+
+    __SEAMLESS_PROPERTIES__ = {
+        "maxUploadSize" : {"path" : "maxUploadSize"},
+        "random" : {"path" : "doesnt.exist"},
+        "services" : {"path" : "services", "wrapper": makeSeamlessService},
+        "collection_policy" : {"path" : "collectionPolicy", "wrapper" : makeCP, "unwrapper" : extractCP}
+    }
+
+    def __init__(self, raw=None):
+        super(SeamlessService, self).__init__(raw)
+
+    @property
+    def data(self):
+        return self.__seamless__.data
--- sword3common/test/unit/test_service.py
+++ sword3common/test/unit/test_service.py
@@ -1,15 +1,48 @@
 from unittest import TestCase
 
-from sword3common.models.service import ServiceDcument
+from sword3common.models.service import ServiceDocument, SeamlessService
 from sword3common.test.fixtures.service import ServiceFixtureFactory
+from sword3common.lib.seamless import SeamlessException, SeamlessData
 
 class TestService(TestCase):
     def test_01_service(self):
         # construct a blank one
-        s = ServiceDcument()
+        s = ServiceDocument()
 
         # a full one
         source = ServiceFixtureFactory.service_document()
-        s = ServiceDcument(source)
+        s = ServiceDocument(source)
 
         assert s.maxUploadSize == 16777216000
+
+    def test_02_seamless(self):
+        s = SeamlessService()
+
+        # a full one
+        source = ServiceFixtureFactory.service_document()
+        try:
+            s = SeamlessService(source)
+
+            assert s.maxUploadSize == 16777216000
+            assert s.random is None
+
+            with self.assertRaises(AttributeError):
+                assert s.whatever == "hello"
+
+            subs = s.services
+            assert len(subs) == 1
+            assert isinstance(subs[0], SeamlessService)
+
+            cp = s.collection_policy
+            assert isinstance(cp, SeamlessData)
+
+            cp.data["description"] = "a new description"
+            s.collection_policy = cp
+            cp2 = s.collection_policy
+            assert cp2.data["description"] == "a new description"
+
+            s.maxUploadSize = 6
+            assert s.maxUploadSize == 6
+
+        except SeamlessException as e:
+            raise Exception(e.message)
