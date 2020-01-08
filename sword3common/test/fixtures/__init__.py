9b9df3931bfbe4eb82254c1f539808b5db20efd5
Richard Jones <richard@cottagelabs.com>
2020-01-08 08:49:17 +0000

    some more import path massaging for fixtures

---
+++ sword3common/test/fixtures/__init__.py
@@ -0,0 +1,3 @@
+from sword3common.test.fixtures.metadata import MetadataFixtureFactory
+from sword3common.test.fixtures.service import ServiceFixtureFactory
+from sword3common.test.fixtures.status import StatusFixtureFactory
