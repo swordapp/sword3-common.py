a8a5a175a20bc31e5dcc33a01d8d519044e8c9ed
Alex Dutton <alex@cottagelabs.com>
2020-01-30 11:36:00 +0000

    Add base and metaclass for SWORD exceptions
    The class method can be used to look up SWORD exception superclasses by status
    code and error name.

--- sword3common/exceptions.py
+++ sword3common/exceptions.py
@@ -1,1 +1,26 @@
-from sword3common.lib.seamless import SeamlessException
+from __future__ import annotations
+
+from http import HTTPStatus
+from typing import Dict, Tuple, Type
+
+from .lib.seamless import SeamlessException
+
+
+class SwordExceptionMeta(type):
+    def __new__(mcs, name, bases, attrs):
+        cls = super().__new__(mcs, name, bases, attrs)
+        if hasattr(cls, "status_code") and hasattr(cls, "name"):
+            cls._registry[(cls.status_code, cls.name)] = cls
+        return cls
+
+
+class SwordException(Exception, metaclass=SwordExceptionMeta):
+    _registry: Dict[Tuple[int, str], Type[SwordException]] = {}
+
+    status_code: int
+    name: str
+    reason: str
+
+    @classmethod
+    def for_status_code_and_name(cls, status_code: int, name: str):
+        return cls._registry[(status_code, name)]
